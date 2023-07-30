bf0de93f3331fba6161a304826f6b72ed18b51e4
Zalim <140335472+sonuzalim@users.noreply.github.com>
2023-07-30 22:50:38 +0300

    Create zALIM.py

---
+++ zALIM.py
@@ -0,0 +1,134 @@
+try:
+totaldmp = 0
+requests,random,sys,json,os,re,datetime,socket,pprint
+logo = """____________ ZALIM____________
+import os
+if __name__ == '__main__':
+head = {"user-agent": "Mozilla/5.0 (Linux; Android 6.0.1; Redmi 4A Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.92 Mobile Safari/537.36","referer": "https://www.facebook.com/","host": "business.facebook.com","origin": "https://business.facebook.com","upgrade-insecure-requests" : "1","accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7","cache-control": "max-age=0","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8","content-type":"text/html; charset=utf-8"}
+github   : not found                                
+from time import sleep as slp
+from os import system as cmd
+except:
+def main():
+def login():
+def grep(f):
+def Jadu():
+count = 0
+_____________________________________ """
+_____________________________________
+YouTube  : no           
+Whatsapp : 0570833776                            
+Facebook : Badshah Zælîm                   
+                                                 [DEAD USER]
+    
+
+	try:
+	print(now.strftime("%y-%m-%d %H:%M:%S"))
+	print(logo)
+	print(47*"\033[1;37;1m-")
+	print("its only for fun")
+	print("ip address is :", ipaddress)
+	print("country : Pakistan")
+	print("_____________________________________")
+	print("[>>] Separating Successful ")
+	print("[>>] New File Save \033[1;32;1m" + o)
+	print("Date and Time ")
+	print(" [1] login with cookies")
+	pass
+	p = input(" Select An Option : ")
+	os.system("xdg-open https://youtube.com/channel/UCrfVEu_7ylJI8XiVBrg4gmw")
+	os.system("clear")
+	os.remove('temp.txt')
+	os.mkdir('Data')
+	o = input('\033[0;97m[->] Save As : \033[1;32;1m')
+	now = datetime.datetime.now()
+	main()
+	ipaddress = socket.gethostbyname(hostnm)
+	input("[>>] Press Inter to go Back < ")
+	if p in ['1','01']:
+	hostnm = socket.gethostname()
+	global totaldmp,count
+	for links in range(ask_link):
+	fbidz = []
+	except:
+	except FileNotFoundError:
+	except Exception as e:
+	Jadu()
+		try:
+		token=open('data/token.txt','r').read()
+		token = open('data/token.txt','r').read()
+		slp(2)
+		slp(1)
+		print(now.strftime("%y-%m-%d %H:%M:%S"))
+		print(logo)
+		print(f"[*] Error {e}")
+		print(47*"\033[1;37;1m-")
+		print("ip address is :", ipaddress)
+		print("_____________________________________")
+		print("[>>] Error : %s"%e)
+		print("[*] Login Not Found")
+		print("Date and Time ")
+		print("")
+		os.system('cat '+f+' | grep "'+li+'" >> '+o)
+		os.system("rm -f data/token.txt")
+		os.system("clear")
+		open("data/token.txt", "w").write(eaag.group(1))
+		open("data/cookie.txt", "w").write(fbcokis)
+		now = datetime.datetime.now()
+		main()
+		login()
+		li = input('[✓] Separate Object : \033[1;32;1m')
+		ipaddress = socket.gethostbyname(hostnm)
+		if ch_x1 in ["yes","Yes","YES","Y","y"]:
+		hostnm = socket.gethostname()
+		head = {"user-agent": "Mozilla/5.0 (Linux; Android 6.0.1; Redmi 4A Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.92 Mobile Safari/537.36","referer": "https://www.facebook.com/","host": "business.facebook.com","origin": "https://business.facebook.com","upgrade-insecure-requests" : "1","accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7","cache-control": "max-age=0","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8","content-type":"text/html; charset=utf-8"}
+		ftoken = requests.get("https://business.facebook.com/business_locations", headers=head, cookies = {"cookie":fbcokis}).text
+		for fbuid in fbidz:
+		filepath = input("[>>] Enter File Path : ")
+		fbcokis= input('[>>] Input Your Facebook Cookie : ')
+		fbcokis = open("data/cookie.txt", "r").read()
+		exit("")
+		except KeyError:
+		else:
+		eaag = re.search("(EAAG\w+)",str(ftoken))
+		completed = 0
+		cmd('rm -rf data/token.txt')
+		cmd('clear')
+		ch_x1 = input("[->] DoYou Want to Use DuplicateID Cuter (n/y) : ")
+		ask_link = int(input('[>>] Enter Grip ID Limit : \033[1;32;1m'))
+		ask_link = 1
+		apnd.close()
+		apnd = open(filepath,'w')
+			try:
+			print('\n')
+			print("cookies already login")
+			print("[*] Public ID Not Found")
+			os.system('sort -r '+filepath+' | uniq > '+newfile)
+			newfile = input("[->] File Without Duplicate ID Save As : ")
+			main()
+			if ch_x2 in ["yes","Yes","YES","Y","y"]:
+			for idnm in dmp['friends']['data']:
+			fbbuid = input("[->] Enter Public ID Link : ")
+			except KeyError:
+			else:
+			dmp = requests.get("https://graph.facebook.com/"+fbbuid+"?fields=friends.limit(5000)&access_token="+token,cookies = {"cookie":fbcokis}).json()
+			count += 1
+			ch_x2 = input("[->] DoYou Want to Use ID Separator (n/y) : ")
+			ch_x2 = input("[->] Do You Want to Use ID Separator (n/y) : \033[1;32;1m")
+				totaldmp+=1
+				print(47*'\033[1;37;1m-')
+				print(47*'-')
+				print(47*"-")
+				print('\x1b[1;91m[>>] Dumping UID From : ' + fbuid)
+				print("\x1b[1;92m[>>] Dumping UID From : " + fbuid)
+				print (f"\x1b[0;37m Your Dump File Save As :\x1b[1;92m {newfile} \x1b[0;37m")
+				print (f"\x1b[0;37m Your Dump File Save As :\x1b[1;92m {filepath} ")
+				print (f"\x1b[0;37m Total ID Dump :\x1b[1;92m {totaldmp}")
+				menu()
+				input("[>>] Press Inter to go Back < ")
+				grep(newfile)
+				grep(filepath)
+				for idnm in dmp['friends']['data']:
+				fbidz.append(idnm['id'])
+				dmp = requests.get("https://graph.facebook.com/"+fbuid+"?fields=friends.limit(5000)&access_token="+token,cookies = {"cookie":fbcokis}).json()
+					apnd.write(idnm['id']+"|"+idnm['name']+'\n')
